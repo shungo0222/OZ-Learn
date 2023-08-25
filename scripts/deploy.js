5d5efe5ed56ed6b7b8c45baa496047e25192bb5f
shungo0222 <kimura.shungo@gmail.com>
2023-08-25 20:38:46 +0900

    Modify "scripts/deploy.js"

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,31 @@
 // scripts/deploy.js
 async function main () {
   // We get the contract to deploy
+  // const Box = await ethers.getContractFactory('Box');
+  // console.log('Deploying Box...');
+  // const box = await Box.deploy();
+  // await box.deployed();
+  // console.log('Box deployed to:', box.address);
+
+  // Retrieve accounts from the local node
+  const accounts = await ethers.provider.listAccounts();
+  console.log(accounts);
+
+  // Set up an ethers contract, representing our deployed Box instance
+  const address = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
   const Box = await ethers.getContractFactory('Box');
-  console.log('Deploying Box...');
-  const box = await Box.deploy();
-  await box.deployed();
-  console.log('Box deployed to:', box.address);
+  const box = await Box.attach(address);
+
+  // Call the retrieve() function of the deployed Box contract
+  const value1 = await box.retrieve();
+  console.log('Box value is', value1.toString());
+
+  // Send a transaction to store() a new value in the Box
+  await box.store(23);
+
+  // Call the retrieve() function of the deployed Box contract
+  const value2 = await box.retrieve();
+  console.log('Box value is', value2.toString());
 }
 
 main()
